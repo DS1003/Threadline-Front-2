11d2b3e75fcd5a16879279c04ebd337f06e27702
babaissandiaye20 <babaissandiaye31@gmail.com>
2024-10-16 22:43:42 +0000

    stories--v4

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -6,15 +6,15 @@ import UserStories from './UserStories';
 const user = JSON.parse(localStorage.getItem('user'));
 
 const StoryCircle = ({ user, image, isUser, onAddStory, onViewStory }) => (
-  <div className="flex-shrink-0   w-24 sm:w-28 transition-transform duration-300 hover:scale-105">
+  <div className="flex-shrink-0 w-24 sm:w-28 transition-transform duration-300 hover:scale-105">
     <button
       onClick={isUser ? onAddStory : onViewStory}
-      className="w-full  h-36 sm:h-44 relative rounded-xl overflow-hidden group"
+      className="w-full h-36 sm:h-44 relative rounded-xl overflow-hidden group"
     >
       <img
         src={image}
         alt={`${user}'s story`}
-        className="w-full h-full  object-cover transition-transform duration-300 group-hover:scale-110"
+        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
       />
       <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-black/60" />
     
@@ -371,13 +371,6 @@ const Stories = () => {
         ref={carouselRef}
         className="flex space-x-4 overflow-x-hidden scroll-smooth"
       >
-        <StoryCircle
-          user="Vous"
-          image={user.photoUrl}
-          isUser={true}
-          onAddStory={() => setIsAddModalOpen(true)}
-          onViewStory={() => {}}
-        />
         <UserStories />
         {displayedStories.map((story) => (
           <StoryCircle
